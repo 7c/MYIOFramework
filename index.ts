8483329f41239974cd4b52c47c19dc65c8243a5e
Your Name <you@example.com>
2024-09-11 09:36:38 -0400

    refactoring

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
+import { Socket } from 'socket.io';
 import { MYIOServer } from './MYIOServer.js';
 
 const { MYIOClient } = require('./MYIOClient.js');
 
-
 export { MYIOServer, MYIOClient }
+
+declare module 'socket.io' {
+    interface Socket {
+        admin?: any;
+        ip?: string;
+        browser?: string;
+        
+    }
+}
